32e2354b0df1618f2127e382e51d75ec8051d37f
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-03-03 23:41:30 +0100

    fix postgres connection, autocreate models, create user model

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/user.model';
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: 'postgres',
+        port: 5432,
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        models: [User],
+        synchronize: true,
+        autoLoadModels: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './user.model';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+@Module({
+  imports: [SequelizeModule.forFeature([User])],
+  exports: [SequelizeModule],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+export class UsersModule {}
