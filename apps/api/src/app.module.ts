251187e113ba390e4070272cab43a956d69b44c8
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-03-04 15:42:48 +0100

    move db config to db module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,24 +6,26 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users/user.model';
+import { DatabaseModule } from './database/database.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    SequelizeModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        dialect: 'postgres',
-        host: 'postgres',
-        port: 5432,
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        models: [User],
-        synchronize: true,
-        autoLoadModels: true,
-      }),
-      inject: [ConfigService],
-    }),
+    DatabaseModule,
+    // SequelizeModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     dialect: 'postgres',
+    //     host: 'postgres',
+    //     port: 5432,
+    //     username: configService.get('POSTGRES_USER'),
+    //     password: configService.get('POSTGRES_PASSWORD'),
+    //     database: configService.get('POSTGRES_DB'),
+    //     models: [User],
+    //     synchronize: true,
+    //     autoLoadModels: true,
+    //   }),
+    //   inject: [ConfigService],
+    // }),
     UsersModule,
   ],
   controllers: [AppController],
---
+++ apps/api/src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from '../users/user.model';
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: 5432,
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        models: [User],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
