6277d425995d577ab4cc632c6f1c605949e745a2
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-03-06 11:47:51 +0100

    extend jwt expiration

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { UsersModule } from '../users/users.module';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '7d' },
     }),
   ],
   providers: [AuthService, PasswordStrategy, JwtStrategy],
