223f41031c7c99ddb1e460779831703f71594b97
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-04-11 23:37:52 +0200

    user model has uuid id and unique index

--- apps/api/src/users/user.model.ts
+++ apps/api/src/users/user.model.ts
@@ -1,10 +1,32 @@
-import { Table, Column, Model } from 'sequelize-typescript';
-
-@Table
+import {
+  Table,
+  Column,
+  Model,
+  IsUUID,
+  PrimaryKey,
+  Unique,
+  BeforeCreate,
+} from 'sequelize-typescript';
+import { v4 as uuidv4 } from 'uuid';
+@Table({
+  timestamps: true,
+})
 export class User extends Model<User> {
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  id: string;
+
+  @Unique
   @Column
   name: string;
 
   @Column
   password: string;
+
+  @BeforeCreate
+  static addUnicorn(instance: User) {
+    // this will also be called when an instance is created
+    instance.id = uuidv4();
+  }
 }
